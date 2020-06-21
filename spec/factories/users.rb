ce5c93600a4d1e2fc8cd6d52b16f2800b5dc5dfd
今福沙紀 <imafukusaki@mba.local>
2020-06-21 22:06:10 +0900

    relationshipのテスト作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,8 @@ FactoryBot.define do
     sequence(:name, "name_1")
     sequence(:email, "example_1@example.com")
     password { 'password' }
+    after(:build) do |user|
+      user.avatar.attach(io: File.open('app/assets/images/avatar.png'), filename: 'avatar.png', content_type: 'img/png')
+    end
   end
 end
