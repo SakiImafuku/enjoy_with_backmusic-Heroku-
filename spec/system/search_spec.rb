6780ea3720fe3822d4137bb1501c8a723be5c7cc
今福沙紀 <imafukusaki@mba.local>
2020-06-27 22:41:05 +0900

    rubocopによる修正

--- spec/system/search_spec.rb
+++ spec/system/search_spec.rb
@@ -5,15 +5,14 @@ describe 'Search', type: :system, js: true do
   let!(:user_b) { create(:user, name: 'ユーザーB', email: 'b@example.com') }
   let!(:taxonomy_composer) { create(:taxonomy, name: "作曲家", position: 1) }
   let!(:taxonomy_instrument) { create(:taxonomy, name: "演奏楽器", position: 2) }
-  let!(:taxon_composer_a) { create(:taxon, name: "作曲家A", taxonomy_id: taxonomy_composer.id)}
-  let!(:taxon_composer_b) { create(:taxon, name: "作曲家B", taxonomy_id: taxonomy_composer.id)}
+  let!(:taxon_a) { create(:taxon, name: "作曲家A", taxonomy_id: taxonomy_composer.id) }
+  let!(:taxon_b) { create(:taxon, name: "作曲家B", taxonomy_id: taxonomy_composer.id) }
   let!(:musicpost_a) { create(:musicpost, title: 'テストA', user_id: user_a.id, overview: "概要A") }
   let!(:musicpost_b) { create(:musicpost, title: 'テストB', user_id: user_b.id, overview: "概要B") }
-  let!(:classification_a) { create(:classification, musicpost_id: musicpost_a.id, taxon_id: taxon_composer_a.id) }
-  let!(:classification_b) { create(:classification, musicpost_id: musicpost_b.id, taxon_id: taxon_composer_b.id) }
+  let!(:class_a) { create(:classification, musicpost_id: musicpost_a.id, taxon_id: taxon_a.id) }
+  let!(:class_b) { create(:classification, musicpost_id: musicpost_b.id, taxon_id: taxon_b.id) }
   let!(:comment) { create(:comment, musicpost_id: musicpost_a.id, content: "テストコメントです") }
   let!(:memo) { create(:memo, musicpost_id: musicpost_a.id, memo: "テストメモです") }
-
 
   before do
     login_for_system(user_a)
@@ -64,5 +63,4 @@ describe 'Search', type: :system, js: true do
     expect(page).to have_content musicpost_a.title
     expect(page).not_to have_content musicpost_b.title
   end
-
 end
