1daac99d1a119061c48c1088f5431f6bce45a17e
今福沙紀 <imafukusaki@mba.local>
2020-06-30 06:53:08 +0900

    musicpost削除機能の実装

--- app/controllers/musicposts_controller.rb
+++ app/controllers/musicposts_controller.rb
@@ -5,4 +5,14 @@ class MusicpostsController < ApplicationController
     @instrument = @musicpost.taxons_filter('演奏楽器')
     @comments = @musicpost.comments.includes(user: :avatar_attachment).order(created_at: :desc)
   end
+
+  def destroy
+    Musicpost.find(params[:id]).destroy
+    flash[:success] = "投稿を削除しました"
+    if request.referrer == musicpost_path
+      redirect_to root_url
+    else
+      redirect_to request.referrer || root_url
+    end
+  end
 end
