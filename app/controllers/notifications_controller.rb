3f6831b5d5bd39ed1e8327a54c41669b1c6163cb
SakiImafuku <s.napustiti@gmail.com>
2020-07-30 18:40:07 +0900

    n+1問題の解決

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications.page(params[:page]).per(20)
+    @notifications = current_user.passive_notifications.page(params[:page]).per(20).includes(:musicpost, :comment, visitor: { avatar_attachment: :blob })
+
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
